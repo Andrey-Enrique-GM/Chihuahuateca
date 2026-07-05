11cab3e56dd9420c06ebf680888086869acd5af6
Andrey <andreyenrique132@gmail.com>
2026-07-04 21:54:52 -0700

    Tercero, intento de llevar la base de datos local a una en la nube (falta arreglar la conexion)

--- persistence/db.py
+++ persistence/db.py
@@ -1,10 +1,15 @@
+import os
 import pymysql
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def get_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="000",
-        database="chihuahuadb",
-        port=3307
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
